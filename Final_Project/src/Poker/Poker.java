389385cd909441339f92f3eacd66937261b3914f
aluesss <70830630+aluesss@users.noreply.github.com>
2024-04-07 21:51:56 -0400

    Update Poker.java

--- Final_Project/src/Poker/Poker.java
+++ Final_Project/src/Poker/Poker.java
@@ -6,8 +6,6 @@ public class Poker{
 	//扑克牌表达形式为点数+花色，点数为123456789,10为T，剩下的JQKA。
 	//花色为C（club 草花） D（diamond 方片) S（spade 黑桃） H（heart 红桃)
 	public static Map<String, String> map = new HashMap<>();
-	public static int[] pair = {0, 0, 0};
-    public static int[] pairOne = {0, 0, 0, 0};
 	//获取点数
 	private static ArrayList<Integer> splitNum(String cards) {
         cards =  cards.replaceAll("T", "10");
@@ -39,7 +37,6 @@ public class Poker{
 		Collections.sort(pokers);
 		for(int i = 1; i < pokers.size(); i++) {
 			if(pokers.get(i).equals(pokers.get(i - 1))) {
-				pairOne[0] = pokers.get(i);
 				return 2;
 			}
 		}
@@ -86,48 +83,62 @@ public class Poker{
 	private static int straight(String cards) {
 		ArrayList<Integer> pokers = splitNum(cards);
         Collections.sort(pokers);
+        int count = 1;
         for (int i = 1; i < pokers.size(); i++) {
         	if (pokers.get(i) - pokers.get(i - 1) != 1) {
-        		return 0;
+        		break;
         	}
+        	else {
+        		count ++;
+        	}
+        	
         }
-        return 5;
-	}
-	//同花
-	private static int flush(String cards) {
-		ArrayList<String> pokers = splitColor(cards);
-		for (int i = 1; i < pokers.size(); i++) {
-			if (!pokers.get(i).equals(pokers.get(i - 1))) {
-                return 0;
-            }
-        }
-		return 6;
-	}
-	//葫芦
-	private static int fullhouse(String cards) {
-		ArrayList<Integer> pokers = splitNum(cards);
-        Collections.sort(pokers);
-        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
-        for (int value : pokers) {
-            frequencyMap.put(value, frequencyMap.getOrDefault(value, 0) + 1);
-        }
-        boolean hasThree = false;
-        boolean hasPair = false;
-        for (int count : frequencyMap.values()) {
-            if (count == 3) {
-            	hasThree = true;
-            }
-            if (count == 2) {
-                hasPair = true;
-            }
-        }
-        if (hasThree && hasPair) {
-        	return 7;
+        if (count >= 5){
+        	return 5;
         }
         else {
         	return 0;
         }
-        
+	}
+	//同花
+	private static int flush(String cards) {
+		ArrayList<String> pokers = splitColor(cards);
+		HashMap<String, Integer> frequencyMap = new HashMap<>();
+		for (String color : pokers) {
+	        frequencyMap.put(color, frequencyMap.getOrDefault(color, 0) + 1);
+	    }
+		for (Integer value : frequencyMap.values()) {
+			if (value >= 5){
+				return 6;
+			}
+		}
+		return 0;
+	}
+	//葫芦
+	private static int fullhouse(String cards) {
+		ArrayList<Integer> pokers = splitNum(cards);
+	    Collections.sort(pokers);
+	    HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+	    for (int value : pokers) {
+	        frequencyMap.put(value, frequencyMap.getOrDefault(value, 0) + 1);
+	    }
+	    boolean hasThree = false;
+	    boolean hasPair = false;
+	    for (int count : frequencyMap.values()) {
+	        if (count == 3) {
+	        	hasThree = true;
+	        }
+	        if (count == 2) {
+	            hasPair = true;
+	        }
+	    }
+	    if (hasThree && hasPair) {
+	    	return 7;
+	    }
+	    else {
+	    	return 0;
+	    }
+	    
 	}
 	//四条
 	private static int fourofKind(String cards) {
@@ -165,10 +176,258 @@ public class Poker{
         list.add(twopair(cards));
         list.add(pair(cards));
         list.add(1);
-        System.out.println(list);
+        //System.out.println(list);
         Collections.sort(list);
         return list.get(list.size() - 1);
+	}
+	//计算牌总点数
+	private int sumPoint(ArrayList<Integer> list) {
+        int sum = 0;
+        for (int i = 0; i < list.size(); i++) {
+            sum += list.get(i);
+        }
+        return sum;
     }
+	//输入两个玩家的牌型，比较出胜者
+	public String compare_winner(String P1, String P2) {
+		int p1 = check(P1);
+		int p2 = check(P2);
+		if (p1 > p2) {
+			return "Player1 wins";
+		}
+		else if (p2 > p1) {
+			return "Player2 wins";
+		}
+		else {
+			ArrayList<Integer> p1_num = splitNum(P1);
+            ArrayList<Integer> p2_num = splitNum(P2);
+            Collections.sort(p1_num);
+            Collections.sort(p2_num);
+            //顺子，同花顺，比较最大牌即可
+            if(p1 == 9 || p1 == 5) {
+            	int r1 = p1_num.get(p1_num.size() - 1);
+                int r2 = p2_num.get(p2_num.size() - 1);
+                if (r1 > r2) {return "Player1 wins";}
+                else if (r2 > r1){return "Player2 wins";}
+                else {return "Tie";}
+            }
+            //同花和高牌，挨张比较
+            if(p1 == 1 || p1 == 6) {
+            	Collections.reverse(p1_num);
+            	Collections.reverse(p2_num);
+            	for(int i = 0; i < p1_num.size(); i++) {
+            		int v1 = p1_num.get(i), v2 = p2_num.get(i);
+            		if(v1 > v2) {return "Player1 wins";}
+	            	else if(v1 < v2) {return "Player2 wins";};
+            	}
+            	return "Tie";
+            }
+            HashMap<Integer, Integer> fre_P1 = new HashMap<>();
+            HashMap<Integer, Integer> fre_P2 = new HashMap<>();
+    	    for (int value : p1_num) {
+    	    	fre_P1.put(value, fre_P1.getOrDefault(value, 0) + 1);
+    	    }
+    	    for (int value : p2_num) {
+    	    	fre_P2.put(value, fre_P2.getOrDefault(value, 0) + 1);
+    	    }
+    	    //葫芦，先比三张再比二张
+    	    if(p1 == 7) {
+    	    	int p1_three = 0, p1_two = 0;
+    	    	int p2_three = 0, p2_two = 0;
+    	    	for (int key : fre_P1.keySet()) {
+    	            int count = fre_P1.get(key);
+    	            if (count == 3) {
+    	                p1_three = key;
+    	            }
+    	            if (count == 2) {
+    	                p1_two = key;
+    	            }
+    	        }
+    	    	for (int key : fre_P2.keySet()) {
+    	            int count = fre_P2.get(key);
+    	            if (count == 3) {
+    	                p2_three = key;
+    	            }
+    	            if (count == 2) {
+    	                p2_two = key;
+    	            }
+    	        }
+    	    	if (p1_three > p2_three) {
+    	    		return "Player1 wins";
+    	        } else if (p1_three < p2_three) {
+    	        	return "Player2 wins";
+    	        } else {
+    	            // 三张一样，比较两张
+    	            if (p1_two > p2_two) {
+    	            	return "Player1 wins";
+    	            } else if (p1_two < p2_two) {
+    	            	return "Player2 wins";
+    	            } else {
+    	            	return "Tie";
+    	            }
+    	        }
+    	    }
+    	    //四条，先比四张再比一张
+    	    if(p1 == 8) {
+    	    	int p1_four = 0, p1_one = 0;
+    	    	int p2_four = 0, p2_one = 0;
+    	    	for (int key : fre_P1.keySet()) {
+    	            int count = fre_P1.get(key);
+    	            if (count == 4) {
+    	            	p1_four = key;
+    	            }
+    	            if (count == 1) {
+    	            	p1_one = key;
+    	            }
+    	        }
+    	    	for (int key : fre_P2.keySet()) {
+    	            int count = fre_P2.get(key);
+    	            if (count == 4) {
+    	            	p2_four = key;
+    	            }
+    	            if (count == 1) {
+    	            	p2_one = key;
+    	            }
+    	        }
+    	    	if (p1_four > p2_four) {
+    	    		return "Player1 wins";
+    	        } else if (p1_four < p2_four) {
+    	        	return "Player2 wins";
+    	        } else {
+    	            // 四张一样，比较一张
+    	            if (p1_one > p2_one) {
+    	            	return "Player1 wins";
+    	            } else if (p1_one < p2_one) {
+    	            	return "Player2 wins";
+    	            } else {
+    	            	return "Tie";
+    	            }
+    	        }
+    	    }
+    	    //三条，先比三张再比剩下的两张
+    	    if(p1 == 4) {
+    	    	int p1_three = 0, p2_three = 0;
+    	    	ArrayList<Integer> p1_ones = new ArrayList<>();
+    	    	ArrayList<Integer> p2_ones = new ArrayList<>();
+    	    	for (int key : fre_P1.keySet()) {
+    	            int count = fre_P1.get(key);
+    	            if (count == 3) {
+    	            	p1_three = key;
+    	            }
+    	            if (count == 1) {
+    	            	p1_ones.add(key);
+    	            }
+    	        }
+    	    	for (int key : fre_P2.keySet()) {
+    	            int count = fre_P2.get(key);
+    	            if (count == 3) {
+    	            	p2_three = key;
+    	            }
+    	            if (count == 1) {
+    	            	p2_ones.add(key);
+    	            }
+    	        }
+    	    	Collections.sort(p1_ones);
+    	    	Collections.sort(p2_ones);
+    	    	Collections.reverse(p1_ones);
+    	    	Collections.reverse(p2_ones);
+    	    	if (p1_three > p2_three) {
+    	    		return "Player1 wins";
+    	        } else if (p1_three < p2_three) {
+    	        	return "Player2 wins";
+    	        } else {
+    	            for(int i = 0; i < p1_ones.size(); i++) {
+    	            	int v1 = p1_ones.get(i), v2 = p2_ones.get(i);
+    	            	if(v1 > v2) {return "Player1 wins";}
+    	            	else if(v1 < v2) {return "Player2 wins";}
+    	            }
+    	            return "Tie";
+    	        }
+    	    }
+    	    //两对
+    	    if(p1 == 3) {
+    	    	ArrayList<Integer> p1_pairs = new ArrayList<>();
+    	    	ArrayList<Integer> p2_pairs = new ArrayList<>();
+    	    	int p1_one = 0, p2_one = 0;
+    	    	for (int key : fre_P1.keySet()) {
+    	            int count = fre_P1.get(key);
+    	            if (count == 2) {
+    	            	p1_pairs.add(key);
+    	            }
+    	            if (count == 1) {
+    	            	p1_one = key;
+    	            }
+    	        }
+    	    	for (int key : fre_P2.keySet()) {
+    	            int count = fre_P2.get(key);
+    	            if (count == 2) {
+    	            	p2_pairs.add(key);
+    	            }
+    	            if (count == 1) {
+    	            	p2_one = key;
+    	            }
+    	        }
+    	    	Collections.sort(p1_pairs);
+    	    	Collections.reverse(p1_pairs);
+    	    	Collections.sort(p2_pairs);
+    	    	Collections.reverse(p2_pairs);
+    	    	//比较对子
+    	    	for(int i = 0; i < p1_pairs.size(); i++) {
+	            	int v1 = p1_pairs.get(i), v2 = p2_pairs.get(i);
+	            	if(v1 > v2) {return "Player1 wins";}
+	            	else if(v1 < v2) {return "Player2 wins";}
+	            }
+    	    	//比较散牌
+    	    	if(p1_one > p2_one) {return "Player1 wins";}
+    	    	else if(p1_one < p1_one) {return "Player2 wins";}
+    	    	else {return "Tie";}
+    	    }
+    	    //一对
+    	    if(p1 == 2) {
+    	    	ArrayList<Integer> p1_ones = new ArrayList<>();
+    	    	ArrayList<Integer> p2_ones = new ArrayList<>();
+    	    	int p1_pair = 0, p2_pair = 0;
+    	    	for (int key : fre_P1.keySet()) {
+    	            int count = fre_P1.get(key);
+    	            if (count == 2) {
+    	            	p1_pair = key;
+    	            }
+    	            if (count == 1) {
+    	            	p1_ones.add(key);
+    	            }
+    	        }
+    	    	for (int key : fre_P2.keySet()) {
+    	            int count = fre_P2.get(key);
+    	            if (count == 2) {
+    	            	p2_pair = key;
+    	            }
+    	            if (count == 1) {
+    	            	p2_ones.add(key);
+    	            }
+    	        }
+    	    	Collections.sort(p1_ones);
+    	    	Collections.reverse(p1_ones);
+    	    	Collections.sort(p2_ones);
+    	    	Collections.reverse(p2_ones);
+    	    	//比较对子
+    	    	if(p1_pair > p2_pair) {return "Player1 wins";}
+    	    	else if(p1_pair < p2_pair) {return "Player2 wins";}
+    	    	else {
+    	    		//比较散排
+    	    		for(int i = 0; i < p2_ones.size(); i++) {
+    	            	int v1 = p2_ones.get(i), v2 = p2_ones.get(i);
+    	            	if(v1 > v2) {return "Player1 wins";}
+    	            	else if(v1 < v2) {return "Player2 wins";}
+    	            }
+    	    		return "Tie";
+    	    	}
+    	    }
+    	    else {
+    	    	return "Something is wrong, shouldn't reach here";
+    	    }
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 		ArrayList<Integer> res1 = new ArrayList<>();
@@ -179,31 +438,31 @@ public class Poker{
 		System.out.println(res2);
 		int i;
 		//高牌，1
-		i = check("2C 3H 4S TC AH");
+		i = check("2C 3H 4S TC AH 6D 7H");
 		System.out.println(i);
 		//一对，2
-		i = check("2C 2H 4S TC AH");
+		i = check("2C 2H 4S TC AH 6D 7H");
 		System.out.println(i);
 		//两对，3
-		i = check("2C 2H 4S 4C AH");
+		i = check("2C 2H 4S 4C AH 6D 7H");
 		System.out.println(i);
 		//三条，4
-		i = check("2C 2H 2S 4C AH");
+		i = check("2C 2H 2S 4C AH 6D 7H");
 		System.out.println(i);
 		//顺子,5
-		i = check("2C 3H 4S 5C 6H");
+		i = check("2C 3H 4S 5C 6H 6D 7H");
 		System.out.println(i);
 		//同花，6
-		i = check("2C 3C JC 5C KC");
+		i = check("2C 3C JC 5C KC 6D 7H");
 		System.out.println(i);
 		//葫芦，7
-		i = check("2C 2H 2S 5C 5H");
+		i = check("2C 2H 2S 5C 5H 6D 7H");
 		System.out.println(i);
 		//四条，8
-		i = check("2C 2H 2S 2D 4H");
+		i = check("2C 2H 2S 2D 4H 6D 7H");
 		System.out.println(i);
 		//同花顺，9
-		i = check("2C 3C 4C 5C 6C");
+		i = check("2C 3C 4C 5C 6C 6D 7H");
 		System.out.println(i);
 		
 	}
